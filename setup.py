64d3495eeb82b258e5619fe485ca65c164c1b43f
Alex Rubin <ahrubin@ucdavis.edu>
2023-10-04 08:51:35 -0700

    trying pyx

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 from setuptools import setup
 from Cython.Build import cythonize
-from setuptools.extension import Extension
-
-extensions = [
-    Extension(
-        "ptu_decoder.ptu_decoder",  # Name of the Python extension to be built
-        ["ptu_decoder/ptu_decoder.pyx", "ptu_decoder/stream_decoder.c"],  # Source files
-        include_dirs=["ptu_decoder"],  # Directory of your header files
-    )
-]
 
 setup(
-    name='ptu_decoder',
-    ext_modules=cythonize(extensions, annotate=True),
+    ext_modules=cythonize("ptu_decoder/stream_decoder.pyx")
 )
