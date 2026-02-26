c93974a6a7ea92f2a4af931b7053aea1c6860f86
Alex Rubin <alexhrubin@gmail.com>
2026-02-26 13:13:52 -0800

    change pyproject and setup

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
 extensions = [
     Extension(
-        "ptu_decoder.stream_decoder",  # IMPORTANT: Include the package prefix
+        "ptu_decoder.stream_decoder",
         ["ptu_decoder/stream_decoder.pyx"],
         include_dirs=[np.get_include()],
     ),
 ]
 
 setup(
-    name="ptu_decoder",
-    version="0.0.1",
-    packages=find_packages(),
-    install_requires=["numpy"],
     ext_modules=cythonize(extensions, language_level=3),
-    package_data={
-        "ptu_decoder": ["*.pyx", "*.pxd"],
-    },
-    zip_safe=False,  # Required for Cython extensions
 )
